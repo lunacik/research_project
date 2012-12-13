38426b01af292f667e9064563ba7b47601b715d1
lunacik <lunacik1@yahoo.pl>
2012-12-13 17:42:57 +0200

    refactoring

--- file_reader/fileReader.h
+++ file_reader/fileReader.h
@@ -1,5 +1,12 @@
 
 /* functions for reading graphs from files */
 
+#ifndef FILEREADER_H
+#define FILEREADER_H
+
+
 int readGraphFromFile(char * fileName, int edgesList[][2]);
 int getEdgesCount(char * fileName);
+
+
+#endif
